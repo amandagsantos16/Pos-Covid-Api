9db45e8bea45f3ea871bad7b4c5a1c4523f7b003
Amanda Gardinale <amanda.gardinale@hotmail.com>
2022-06-10 18:40:13 -0300

    Adicionado cadastro de horarios do psicologo

--- pos-covid/Controllers/PsicologoController.cs
+++ pos-covid/Controllers/PsicologoController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using pos_covid_api.Data;
@@ -28,7 +29,7 @@ public class PsicologoController : MainController
             AdicionarErroProcessamento("Psicólogo já cadastrado no sistema");
             return CustomResponse();
         }
-        
+
         psicologo = new Psicologo
         {
             Id = Guid.NewGuid(),
@@ -38,10 +39,40 @@ public class PsicologoController : MainController
             Resumo = request.Resumo,
             Especializacoes = request.Resumo
         };
-        
+
         await _context.Psicologos.AddAsync(psicologo);
         await _context.SaveChangesAsync();
 
         return Created("", psicologo);
+    }
+
+    [HttpPost]
+    [Authorize]
+    [Route("horarios")]
+    public async Task<IActionResult> AdicionarHorarios(AdicionarHorariosPsicologo request)
+    {
+        foreach (var horarioRequest in request.Horarios)
+        {
+            foreach (var hora in horarioRequest.Horarios)
+            {
+                var horaFormatada = DateTime.ParseExact(hora, "HH:mm",
+                    CultureInfo.CreateSpecificCulture("pt-BR"));
+
+                var horario = new Horario
+                {
+                    Id = Guid.NewGuid(),
+                    Hora = horaFormatada,
+                    PsicologoId = request.PsicologoId,
+                    DiaDaSemana = (int) horarioRequest.DiaDaSemana
+                };
+
+                await _context.Horarios.AddAsync(horario);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        var retorno = await _context.Horarios.Where(x => x.PsicologoId == request.PsicologoId).ToListAsync();
+
+        return Created("", retorno);
     }
 }
---
+++ pos-covid/ViewModels/AdicionarHorariosPsicologo.cs
@@ -0,0 +1,13 @@
+﻿namespace pos_covid_api.ViewModels;
+
+public class AdicionarHorariosPsicologo
+{
+    public Guid PsicologoId { get; set; }
+    public IEnumerable<AdicionarHorariosPsicologoData> Horarios { get; set; }
+}
+
+public class AdicionarHorariosPsicologoData
+{
+    public DayOfWeek DiaDaSemana { get; set; }
+    public IList<string> Horarios { get; set; }
+}
