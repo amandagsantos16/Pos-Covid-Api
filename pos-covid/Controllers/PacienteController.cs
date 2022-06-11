05f64eeedbd2b7a0be5c3e4690353673e6fadc64
Amanda Gardinale <amanda.gardinale@hotmail.com>
2022-06-11 01:21:34 -0300

    Endpoint para realizar agendamento

--- pos-covid/Controllers/PacienteController.cs
+++ pos-covid/Controllers/PacienteController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using pos_covid_api.Data;
+using pos_covid_api.Enums;
+using pos_covid_api.Models;
+using pos_covid_api.ViewModels;
 
 namespace pos_covid_api.Controllers;
 
@@ -7,5 +11,29 @@ namespace pos_covid_api.Controllers;
 [Route("api/pacientes")]
 public class PacienteController : MainController
 {
-    
+    private readonly ApplicationDbContext _context;
+
+    public PacienteController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpPost]
+    [Route("agendamentos")]
+    public async Task<IActionResult> NovoAgendamento(AdicionarAgendamentoViewModel request)
+    {
+        var agendamento = new Agenda
+        {
+            PacienteId = request.PacienteId,
+            PsicologoId = request.PsicologoId,
+            HorarioId = request.HorarioId,
+            Data = request.Data,
+            StatusAgendamento = EnumStatusAgendamento.Pendente
+        };
+
+        await _context.Agendamentos.AddAsync(agendamento);
+        await _context.SaveChangesAsync();
+
+        return Created("", agendamento);
+    }
 }
---
+++ pos-covid/ViewModels/AdicionarAgendamentoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace pos_covid_api.ViewModels;
+
+public class AdicionarAgendamentoViewModel
+{
+    public Guid? PsicologoId { get; set; }
+    public Guid? PacienteId { get; set; }
+    public Guid? HorarioId { get; set; }
+    public DateTime Data { get; set; }
+}
